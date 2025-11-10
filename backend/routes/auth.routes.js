6d6d9991b64b27f14e770b27a82de6014fbbb531
piyush_alpha01 <pm455567@gmail.com>
2025-11-10 21:04:27 +0530

    01-auth controller added

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { signup,login,logout, getme } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middleware/protectRoute.js";
+const router = express.Router();
+
+router.get("/me",protectRoute,getme)
+router.post("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logout);
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import { connectDB } from "./db/db.js";
 import dotenv from "dotenv"
 import path from "path";
 import { fileURLToPath } from 'url';
-
+import authRoute from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -11,9 +12,12 @@ dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Server is ready");
-})
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+
+app.use("/api/auth",authRoute);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`Sever running on port ${PORT}`);
